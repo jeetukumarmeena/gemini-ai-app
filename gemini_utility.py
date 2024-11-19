23fab3845fba5d71a39347e9018af25de50887c0
Jeetu Kumar Meena <138495217+jeetukumarmeena@users.noreply.github.com>
2024-11-19 23:31:25 +0530

    Updated

--- gemini_utility.py
+++ gemini_utility.py
@@ -1,8 +1,6 @@
-# gemini_utility.py
 import json 
 import os  
 import google.generativeai as genai
-
 
 # Get the working directory
 working_directory = os.path.dirname(os.path.abspath(__file__))
@@ -19,36 +17,28 @@ GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
 # Configure Google Generative AI with the API key
 genai.configure(api_key=GOOGLE_API_KEY)
 
-# function to load the gemini-pro model for chatbot
+# Function to load the gemini-1.5-flash model for chatbot
 def load_gemini_pro_model():
-    gemini_pro_model = genai.GenerationModel("gemini-pro")
+    gemini_pro_model = genai.GenerationModel("gemini-1.5-flash")
     return gemini_pro_model
 
-# function for image captioning
-
-def gemini_pro_vision_response(promt,image):
-    gemini_pro_vision_model = genai.GenerativeModel("gemini-pro-vision")
-    response = gemini_pro_vision_model.generate_content([promt,image ])
-    result =response.text
-    return result
-
-
-
-
-# function to get the embedding for text
-
-def embedding_model_response(input_text):
-    embedding_model = "models/embeddinh-001"
-    embedding = genai.embed_content(model= embedding_model,content=input_text, task_type="retrieval_document")
-    embedding_list = embedding["embedding"]
-    return embedding
-
-
-# function to get the response form gemini-pro LLM
-def gemini_pro_response(user_promt):
-    gemini_pro_model = genai.GenerativeModel("gemini-pro")
-    response = gemini_pro_model.generate_content(user_promt)
+# Function for image captioning
+def gemini_pro_vision_response(prompt, image):
+    gemini_pro_vision_model = genai.GenerativeModel("gemini-1.5-flash")
+    response = gemini_pro_vision_model.generate_content([prompt, image])
     result = response.text
     return result
 
+# Function to get the embedding for text
+def embedding_model_response(input_text):
+    embedding_model = "models/embedding-001"
+    embedding = genai.embed_content(model=embedding_model, content=input_text, task_type="retrieval_document")
+    embedding_list = embedding["embedding"]
+    return embedding
 
+# Function to get the response from gemini-1.5-flash LLM
+def gemini_pro_response(user_prompt):
+    gemini_pro_model = genai.GenerativeModel("gemini-1.5-flash")
+    response = gemini_pro_model.generate_content(user_prompt)
+    result = response.text
+    return result
